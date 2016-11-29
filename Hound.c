28e68e177a87fd0f9e6310c4c6e581d37c21080c
NikhilPappu <nkhlpappu@gmail.com>
2016-11-29 07:20:10 -0500

    Update Hound.c

--- Hound.c
+++ Hound.c
@@ -1,49 +1,57 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#define IN 999
+#define IN 999 //infinity value
 int N = 100;
-struct tile {
-    int hound_dist;
-    int sourceDist;
-    int targetDist;
-    int selected;
-    int type;
-    int prev;
+struct tile { // Structure for a tile on the maze
+    int hound_dist; //Distance from the hound's position.
+    int sourceDist; //Dist from the start point
+    int targetDist; //Dist from the target point
+    int selected; 
+    int type; //Different values for wall,hound and empty tile
+    int prev; //An integer corresponding to the prev tile on the path
     };
-int arrCheck(int arr[],int n,int key){
+int arrCheck(int arr[],int n,int key){ // Function to search an element in an array.
     int i;
     for(i=n-2;i>=0;i--){
      if(arr[i]==key) return 1;   
     }
     return 0;
     }
-int convert(int i,int j){
+int convert(int i,int j){ 
+    // converts coordinates (i,j) into a corresponding number from 1 to N^2.
     return N*i + j;
     }
 
  void printAllPaths(int u,int d, int path[],struct tile tiles[N*N],int path_index,int min[],int *minlen,int target,int minHoundDist,int minExitDist)    //changed
  {
+     /*This recursive function finds all the possible valid paths from the start to the exit point 
+      * It ends the recursion of a path if the path fails to meet the validity criteria.
+      * It also stores the minimum valid path of all the valid paths.
+      * */
         int i;
-        int houndToExit = tiles[target].hound_dist;
+        int houndToExit = tiles[target].hound_dist; //Distance from hound to exit
         tiles[u].selected = 1;
         path[path_index] = u;
         path_index++;
      
-        if(tiles[u].hound_dist<=minHoundDist)                       //changed
+        if(tiles[u].hound_dist<=minHoundDist) //min distance of the hound from the person's path.
         {
             minHoundDist=tiles[u].hound_dist;
             minExitDist = path_index-1;
         }
      
-         if(path_index-1>houndToExit || path_index-1>=(minHoundDist-tiles[u].sourceDist+ceil(minExitDist/2)))       //changed
+         if(path_index-1>houndToExit - tiles[u].sourceDist|| path_index-1>=(minHoundDist-tiles[u].sourceDist+ceil(minExitDist/2)))
+            //Validity condition for a path (refer to the Instructions)
          {
             path_index--;
             tiles[u].selected=0;
-            return;
+            return; //Return from the recursion if the path is invalid
         }
-        else if(u==d)
+        else if(u==d) //start = target ie the person has reached the exit via a valid path
             {
+                //Check if the current valid path is less than the min valid path.
+                //If not make it the minimum.
                 if(path_index<*minlen || *minlen==0){
                     
                     for(i=path_index-1;i>=0;i--){
@@ -59,6 +67,7 @@ int convert(int i,int j){
                 printf("\n\n");
             }
         else
+            //Call the function with the start point being the neighbours of the current start point.
             {
                 if( tiles[u-N].type!=1 && tiles[u-N].selected==0)
                 {
@@ -81,6 +90,7 @@ path_index--;
 tiles[u].selected=0;
 }
 
+//Calculates the minimum distance of the hound from all the tiles on the maze using bfs.
 void houndDistance(int queue[],struct tile tiles[N*N],int hound_start){
     int pos1 = 0,pos2 = 0;
     queue[0] = hound_start;
@@ -115,6 +125,7 @@ void houndDistance(int queue[],struct tile tiles[N*N],int hound_start){
    
     }
 }
+//Calculates the minimum distance of the player's initial pos from all the tiles on the maze using bfs.
 void sourceDistance(int queue[],struct tile tiles[N*N],int source){
     int pos1 = 0,pos2 = 0;
     queue[0] = source;
@@ -149,6 +160,8 @@ void sourceDistance(int queue[],struct tile tiles[N*N],int source){
    
     }
 }
+//Funtion to display all the contents of a file.
+//Called to display the instructions from Instructions.txt
 static void display_file(const char *file_name)
 {
     FILE *f = fopen(file_name, "r");      
@@ -165,10 +178,10 @@ static void display_file(const char *file_name)
 }
 int main(){
     int c;
-    display_file("..\\Instructions.txt");
+    display_file("..\\Instructions_hound.txt");
     do {
     N = 100;
-    struct tile tiles[N*N];
+    struct tile tiles[N*N]; // An array of tile structures
     int min[1000];
     int minlen = 0;
     int queue[1000];
@@ -178,11 +191,11 @@ int main(){
     int hound_start;
     char temp;
     int houndToExit;
-    int minHoundDist=IN;                                            //changed
-    int minExitDist=0;                                              //changed
+    int minHoundDist=IN;                                            
+    int minExitDist=0;                                   
     char f[100];
     char f2[100]="..\\";
-    for(i=0;i<N*N;i++){
+    for(i=0;i<N*N;i++){ //Initialize the attributes for each tile.
         tiles[i].selected = 0;
         tiles[i].prev = -1;
         tiles[i].type = 0;
@@ -196,6 +209,7 @@ int main(){
     fp = fopen(f2,"r");
     fscanf(fp,"%d",&N);
     fgetc(fp);
+    //Initialize the tile type for all the tiles.
     for(i=0;i<N;i++){
         for(j=0;j<N+1;j++){
            temp = fgetc(fp);
@@ -224,17 +238,10 @@ int main(){
     tiles[source].sourceDist = 0;
     houndDistance(queue,tiles,hound_start);
     sourceDistance(queue2,tiles,source);
-//    for(i=0;i<N*N;i++){
-//        printf("%d=%d\n",i,tiles[i].sourceDist);
-//        
-//    }
+    //Print all the valid paths in the form of (i,j) coordinates of each tile on the path.
     printf("\nAll the possible paths are : \n\n");
-    printAllPaths(target,source,path,tiles,0,min,&minlen,target,minHoundDist,minExitDist);   //changed
+    printAllPaths(target,source,path,tiles,0,min,&minlen,target,minHoundDist,minExitDist);
     printf("\n");
-    
-//    for(i=minlen-2;i>=0;i--){
-//        printf("(%d,%d) ",min[i]/N,min[i]%N);
-//        }
     printf("\nThe shortest valid path is : \n\n");
     for(i=0;i<N;i++){
         for(j=0;j<N+1;j++){
